6cdc1fe7a572c9548c4bb500969b8da920b2a016
HaiDang-dz <nguyenhaidangg261103@gmail.com>
2025-10-27 10:10:18 +0700

    thêm

--- QLAccBank/Form/Frm_Add_Customer.cs
+++ QLAccBank/Form/Frm_Add_Customer.cs
@@ -33,7 +33,7 @@ namespace QLAccBank
             {
                 txtHoTen.Enabled = false;
                 btnXacNhan.Text = "Cập nhật";
-                this.Text = "CẬP";
+                this.Text = "";
 
                 txtMaKhachHang.Text = customer.CustomerID;
                 txtHoTen.Text = $"{customer.LastName} {customer.FirstName}";
